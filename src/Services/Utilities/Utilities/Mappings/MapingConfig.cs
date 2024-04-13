c0dd6edb1718f221b2bd76aeb26818ae62cfb331
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-04-13 23:03:36 +0200

    add mapster config file

---
+++ src/Services/Utilities/Utilities/Mappings/MapingConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace Utilities.Mappings;
+
+public class MapingConfig : IRegister
+{
+
+    public void Register(TypeAdapterConfig config)
+    {
+        throw new NotImplementedException();
+    }
+}
