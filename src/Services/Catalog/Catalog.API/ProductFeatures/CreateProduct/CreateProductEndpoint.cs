19625139b7368ad07cd46e338c73dbf579bfc257
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-04-13 23:04:19 +0200

    add complete Create product feature.

---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.ProductFeatures.CreateProduct;
+
+public record CreateProductDto(string Name, string Description, decimal Price, string ImageFile, List<int> CategoryIds);
+
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/products", async (CreateProductDto product, ISender sender) =>
+        {
+            var command = product.Adapt<CreateProductDto>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+          .WithName("Create Product")
+          .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+          .ProducesProblem(StatusCodes.Status400BadRequest)
+          .WithDescription("Create a product");
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Models;
+using Mapster;
+using Utilities.CQRS;
+
+namespace Catalog.API.ProductFeatures.CreateProduct;
+
+public record CreateProductCommand(CreateProductDto product) : ICommand<CreateProductResponse>
+{
+}
+public record CreateProductResponse(Guid Id)
+{
+}
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+{
+    public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // map dto to entity
+        // save entity
+        // return response
+
+        var product = command.product.Adapt<Product>();
+
+        return new CreateProductResponse(product.Id);
+    }
+}
+
