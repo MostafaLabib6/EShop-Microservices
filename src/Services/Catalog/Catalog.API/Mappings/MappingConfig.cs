3e3f114d3524c6b0f0f91f0317054708896a5872
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-06 14:31:21 +0300

    add mapster.

---
+++ src/Services/Catalog/Catalog.API/Mappings/MappingConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Mappings;
+
+public class MappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        //config.NewConfig<CreateProductDto, CreateProductCommand>()
+        //    .Map(dest => dest.product, src => src);
+
+    }
+}
