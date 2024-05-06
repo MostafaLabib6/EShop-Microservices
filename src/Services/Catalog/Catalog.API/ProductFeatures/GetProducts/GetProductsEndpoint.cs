fa952c450331aa20ff207de309f0fe19c004fb8a
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-06 14:31:36 +0300

    add Get product feature.

--- src/Services/Catalog/Catalog.API/ProductFeatures/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProducts/GetProductsEndpoint.cs
@@ -1,5 +1,18 @@
 ﻿namespace Catalog.API.ProductFeatures.GetProducts;
 
-public class GetProductsEndpoint
+public class GetProductsEndpoint : ICarterModule
 {
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products",
+			async (IMediator sender) =>
+			{
+				var result = await sender.Send(new GetProductsQuery());
+				return Results.Ok(result);
+			}).WithDisplayName("GetProducts")
+			.Produces<GetProductsResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithDescription("Get Products");
+
+	}
 }
--- src/Services/Catalog/Catalog.API/ProductFeatures/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProducts/GetProductsHandler.cs
@@ -1,17 +1,18 @@
-﻿using Catalog.API.Models;
-using MediatR;
-using Utilities.CQRS;
-
-namespace Catalog.API.ProductFeatures.GetProducts;
+﻿namespace Catalog.API.ProductFeatures.GetProducts;
 
 public record GetProductsQuery : IQuery<GetProductsResponse>;
-//public record ProductDto(string Name, string Description, decimal Price,string ImageFile, List<string> Category);
-public record GetProductsResponse(IReadOnlyList<Product> Products); // should be a IEnumerable?? 
-internal class GetProductsHandler : IQueryHandler<GetProductsQuery, GetProductsResponse>
+public record GetProductsResponse(IReadOnlyList<ProductDto> Products);
+internal class GetProductsHandler(IDocumentSession _session /*, Logger<GetProductsHandler> _logger*/) : IQueryHandler<GetProductsQuery, GetProductsResponse>
 {
 
-    Task<GetProductsResponse> IRequestHandler<GetProductsQuery, GetProductsResponse>.Handle(GetProductsQuery request, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+	Task<GetProductsResponse> IRequestHandler<GetProductsQuery, GetProductsResponse>.Handle(GetProductsQuery query, CancellationToken cancellationToken)
+	{
+		//_logger.LogInformation("GetProductsHandler.Handle with {@query}", query);
+
+		var products = _session.Query<Product>().ToList();
+
+		var productDtos = products.Adapt<IReadOnlyList<ProductDto>>();
+
+		return Task.FromResult(new GetProductsResponse(productDtos)); // valid case case products already has a value not waiting for it.
+	}
 }
