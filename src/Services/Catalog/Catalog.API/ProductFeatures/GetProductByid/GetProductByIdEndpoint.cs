6117c3604c69602af2612cb491ec4edd7b5e7020
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-07 12:14:01 +0300

    add get product by id feature

---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProductByid/GetProductByIdEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.ProductFeatures.GetProductByid
+{
+	public class GetProductByIdEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("Products/{Id}", async (Guid Id, IMediator sender) =>
+			{
+				var product = await sender.Send(new GetProductByIdQuery(Id));
+
+				return Results.Ok(product);
+			}).WithName("GetProductById")
+		.Produces<ProductDto>(StatusCodes.Status200OK)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.WithSummary("Get Product by Id")
+		.WithDescription("Get Product by Id");
+		}
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProductByid/GetProductByIdHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.ProductFeatures.GetProductByid;
+
+public record GetProductByIdResponse(ProductDto product);
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResponse>;
+
+public class GetProductByIdQueryHandler(ILogger<GetProductByIdQueryHandler> _logger, IDocumentSession _session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResponse>
+{
+	public async Task<GetProductByIdResponse> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+	{
+		_logger.LogInformation($"GetProductByIdQueryHandler.Handle {query!}");
+
+		var product = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+
+		if (product is null)
+			throw new KeyNotFoundException($"Product {query.Id} not found");
+
+		return new GetProductByIdResponse(product.Adapt<ProductDto>());
+	}
+}
