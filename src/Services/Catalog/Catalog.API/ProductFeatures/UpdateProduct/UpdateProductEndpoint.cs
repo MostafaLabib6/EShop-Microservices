67f3aba3beaada8c6c8588c1ec4d5dfdbc8e22f5
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-07 12:14:15 +0300

    add update product feature.

---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.ProductFeatures.UpdateProduct
+{
+	public class UpdateProductEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPut("/products",
+				async (UpdateProductCommand request, IMediator sender) =>
+				{
+					await sender.Send(new UpdateProductCommand(request.Product));
+					return Results.NoContent();
+				}).WithDisplayName("UpdateProduct")
+				.Produces(StatusCodes.Status204NoContent)
+				.ProducesProblem(StatusCodes.Status400BadRequest)
+				.WithDescription("Update Product")
+				.WithSummary("Update Product");
+		}
+
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.API.ProductFeatures.UpdateProduct;
+
+public record UpdateProductCommand(Product Product) : ICommand;
+
+internal class UpdateProductCommandHandler(ILogger<UpdateProductCommandHandler> _logger, IDocumentSession _session) : ICommandHandler<UpdateProductCommand, Unit>
+{
+	public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+	{
+		_logger.LogInformation($"UpdateProductCommandHandler.Handle {request!}");
+
+		_session.Store(request.Product);
+		await _session.SaveChangesAsync(cancellationToken);
+
+		return Unit.Value;
+	}
+}
