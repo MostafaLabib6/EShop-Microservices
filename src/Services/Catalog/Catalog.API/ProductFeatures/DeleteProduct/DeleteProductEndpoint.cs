aa49e50867487be63d85f241d508f446211a7a5d
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-07 12:13:18 +0300

    add delete product feature.

---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.ProductFeatures.DeleteProduct;
+
+public class DeleteProductEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapDelete("/products/{ProductId}",
+			async (Guid ProductId, IMediator sender) =>
+			{
+				await sender.Send(new DeleteProductCommand(ProductId));
+				return Results.NoContent();
+			}
+			).WithDisplayName("DeleteProduct")
+			.Produces(StatusCodes.Status204NoContent)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithDescription("Delete Product")
+			.WithSummary("Delete Product");
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.ProductFeatures.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand;
+
+internal class DeleteProductCommandHandler(ILogger<DeleteProductCommandHandler> _logger, IDocumentSession _session)
+	: ICommandHandler<DeleteProductCommand, Unit>
+{
+	public async Task<Unit> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+	{
+		_logger.LogInformation($"DeleteProductCommandHandler.Handle {command!}");
+
+		_session.DeleteWhere<Product>(x => x.Id == command.Id);
+
+		await _session.SaveChangesAsync();
+
+		return Unit.Value;
+	}
+}
