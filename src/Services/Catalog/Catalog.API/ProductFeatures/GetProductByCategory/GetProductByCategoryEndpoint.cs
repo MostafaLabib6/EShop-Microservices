5a914cace010581657c0c4425d0419a1227550c4
Mostafa Labib <81976413+MostafaLabib6@users.noreply.github.com>
2024-05-07 12:13:38 +0300

    add get product by category feature.

---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.ProductFeatures.GetProductByCategory
+{
+	public class GetProductByCategoryEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("Products/Category/{Category}", async (string Category, IMediator sender) =>
+			{
+				var products = await sender.Send(new GetProductByCategoryQuery(Category));
+
+				return Results.Ok(products);
+			}).WithName("GetProductByCategory")
+			.Produces<List<ProductDto>>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Get Product by Category")
+			.WithDescription("Get Product by Category");
+		}
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/ProductFeatures/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.ProductFeatures.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResponse>;
+
+public record GetProductByCategoryResponse(IEnumerable<ProductDto> Products);
+
+internal class GetProductByCategoryQueryHandler
+	(ILogger<GetProductByCategoryQueryHandler> _logger, IDocumentSession _seesion)
+		: IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResponse>
+{
+	public async Task<GetProductByCategoryResponse> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+	{
+		_logger.LogInformation($"GetProductByCategoryQueryHandler.Handle {query!}");
+
+		var products = await _seesion.Query<Product>()
+			.Where(p => p.Category.Contains(query.Category))
+			.ToListAsync(cancellationToken);
+
+		return new GetProductByCategoryResponse(products.Adapt<IEnumerable<ProductDto>>());
+	}
+}
